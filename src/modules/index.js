aa8620fb886bf7ec10aab389c7947df10213579a
Hartong, Glenn <Glenn.Hartong@signetjewelers.com>
2018-07-03 19:25:05 -0400

    working event details page

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import counter from './counter'
+import event from './event'
 
 export default combineReducers({
   routing: routerReducer,
-  counter
+  counter,
+  event
 })
